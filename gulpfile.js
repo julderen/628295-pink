c77e8d968b182471a089b643756a04421ece5ddf
deniskorokhodkin <korokhodkindenis@gmail.com>
2018-10-12 13:45:11 +0300

    Добавляет стили для range input

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var postcssInputStyle = require("postcss-input-style");
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -12,6 +13,7 @@ gulp.task("css", function () {
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
+      postcssInputStyle(),
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
