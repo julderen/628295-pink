dc23d7521536ad3dc109a602134502e7f63e489e
deniskorokhodkin <korokhodkindenis@gmail.com>
2018-10-23 08:09:06 +0300

    Завершает верстку header

--- source/js/menuToggle.js
+++ source/js/menuToggle.js
@@ -1,19 +1,19 @@
-var menuToggle = document.querySelector('.page-header__toggle-button');
+var logoWrapper = document.querySelector('.page-header__logo-wrapper');
 var menu = document.querySelector('.page-header__menu');
 
 menu.classList.remove('page-header__menu--nojs');
-menuToggle.classList.remove('page-header__toggle-button--nojs');
+logoWrapper.classList.remove('page-header__logo-wrapper--nojs');
 
-menuToggle.addEventListener('click', function() {
+logoWrapper.addEventListener('click', function() {
   if (menu.classList.contains('page-header__menu--close')) {
-    menuToggle.classList.remove('page-header__toggle-button--close');
+    logoWrapper.classList.remove('page-header__logo-wrapper--close');
     menu.classList.remove('page-header__menu--close');
-    menuToggle.classList.add('page-header__toggle-button--open');
+    logoWrapper.classList.add('page-header__logo-wrapper--open');
     menu.classList.add('page-header__menu--open');
   } else {
     menu.classList.add('page-header__menu--close');
-    menuToggle.classList.add('page-header__toggle-button--close');
+    logoWrapper.classList.add('page-header__logo-wrapper--close');
     menu.classList.remove('page-header__menu--open');
-    menuToggle.classList.remove('page-header__toggle-button--open');
+    logoWrapper.classList.remove('page-header__logo-wrapper--open');
   }
 });
