84118f0eb5fdd68eed16fbe1d89b4fa854ab361b
deniskorokhodkin <korokhodkindenis@gmail.com>
2018-10-24 21:48:01 +0300

    Создает спрайт svg

---
+++ source/js/map.js
@@ -0,0 +1,18 @@
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [59.938631, 30.323055],
+    zoom: 16,
+    controls: []
+  });
+
+  var myGeoObject = new ymaps.GeoObject({
+    geometry: {
+      type: "Point",
+      coordinates: [59.938631, 30.323055]
+    }
+  });
+
+  myMap.geoObjects.add(myGeoObject);
+}
